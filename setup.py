1db6abd3fdee1b88cdb98ea383275399c76376da
prathmesh <prathmeshnandurkar123@gmail.com>
2023-02-22 11:12:40 +0530

    added few changes

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
-
+# 3dE*.cd5sYw8bnh
 setup(
-    name='djangviz',
+    name='djangoviz',
     version='1.0.0',
     author='Prathmesh Nandurkar',
     author_email='prathmeshnandurkar123@gmail.com',
@@ -14,6 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.7',
+    py_modules = ['action', 'djangoviz'],
     license='MIT',
     packages=find_packages(),
     install_requires=[requirements],
